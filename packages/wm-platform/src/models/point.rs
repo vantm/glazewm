4bba635095f43edb31a89676e841e586a5fe1e09
Van Tran <tmvan.1903@gmail.com>
2025-10-01 09:41:24 +0700

    feat: improve floating focus
    - floating by directions
    - allow focus monitor in a direction from floating windows

--- packages/wm-platform/src/models/point.rs
+++ packages/wm-platform/src/models/point.rs
@@ -16,4 +16,17 @@ impl Point {
     #[allow(clippy::cast_precision_loss)]
     ((dx * dx + dy * dy) as f32).sqrt()
   }
+
+  #[must_use]
+  pub fn from_xy(x: i32, y: i32) -> Self {
+    Self { x, y }
+  }
+
+  #[must_use]
+  pub fn min() -> Self {
+    Self {
+      x: i32::MIN,
+      y: i32::MIN,
+    }
+  }
 }
